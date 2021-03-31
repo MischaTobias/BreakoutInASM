06413ac8c8b08681fb32b875e5e81a39b42ccb3c
Mischa Tobias <mischa.tobias4b@gmail.com>
2021-03-31 17:23:59 -0600

    Bar movement done (with a bit missing on the sides)

--- CreateBarAutomatic.asm
+++ CreateBarAutomatic.asm
@@ -1,7 +1,3 @@
-// A = Address
-// M = RAM[A]
-// D - dato guardado
-
 @24397
 D=A
 @0 //Dirección guía barra
@@ -15,7 +11,7 @@ D=A
 @2 //Dato por columna a dibujar
 M=D
 
-(BAR)
+(BAR) //12
     @0 //Dirección guía barra
     A=M
     M=-1
@@ -29,7 +25,7 @@ M=D
     @BAR
     0;JMP
 
-(NEXT) //Reiniciar datos para creación de siguiente línea
+(NEXT)//24  //Reiniciar datos para creación de siguiente línea
     @26 // 32-6
     D=A
     @0 //Dirección guía barra
@@ -43,12 +39,12 @@ M=D
     D=M
     @BAR
     D;JGT // Finalización del loop
-    @70 //32+32+6
+    @96 //32+32+32
     D=A
     @0 //Dirección guía barra
     M=M-D
-    /** Restablecimiento de valores para creación de barra */
-    @3
+    // Restablecimiento de valores para creación de barra
+    @3 //41
     D=A
     @1 //Dato por línea a dibujar
     M=D
@@ -57,7 +53,7 @@ M=D
     @2 //Dato por columna a dibujar
     M=D
 
-(LOOP)
+(LOOP)//49
     @KBD
     D=M
     @PRESSED
@@ -68,20 +64,26 @@ M=D
 (PRESSED)
     //130 izquierda
     //132 derecha
-    @KBD
+    @KBD //55
     D=M
-    @4
+    @3
     M=D
     @130
     D=A
-    @4
+    @3
     M=M-D
+    D=M
+    @0
+    D=M
+    @4
+    M=D
+    @3
     D=M
     @LEFT
     D;JEQ
     @2 //Dato por columna a dibujar
     D=A
-    @4
+    @3
     M=M-D
     D=M
     @RIGHT
@@ -89,15 +91,84 @@ M=D
     @LOOP
     0;JMP
 
-(LEFT)
-//BORRAR LAS 3 DE LA DERECHA Y AGREGAR 3 A LA IZQUIERDA
-//VERIFICAR QUE SEA MENOR A 32 (De esa línea en específico) PARA QUE SE QUEDE EN LA MISMA LÍNEA
-@LOOP
-0;JMP
+(LEFT) //81
+//BORRAR LAS 3 DE LA DERECHA Y AGREGAR 3 A LA IZQUIERDA, VERIFICAR QUE SEA MENOR A 32 (De esa línea en específico) PARA QUE SE QUEDE EN LA MISMA LÍNEA
+    @24385
+    D=A
+    @0
+    D=D-M
+    @LOOP
+    D;JEQ
+    @3
+    D=A
+    @4
+    M=M+D
+    //Mover a la izquierda la posición
+    @0
+    M=M-D
+    //Valores para borrar
+    @5
+    M=D
+    @6
+    M=D
+    @BARERASE
+    0;JMP
 
+(RIGHT) //93
+//BORRAR LAS 3 DE LA IZQUIERDA Y AGREGAR 3 A LA DERECHA, VERIFICAR QUE SEA MAYOR A 32 (De esa línea en específico) PARA QUE SE QUEDE EN LA MISMA LÍNEA
+    @24409
+    D=A
+    @0
+    D=D-M
+    @LOOP
+    D;JEQ
+    @3
+    D=A
+    //Mover a la derecha la posición
+    @0
+    M=M+D
+    //Valores para borrar
+    @5
+    M=D
+    @6
+    M=D
+    @BARERASE
+    0;JMP
 
-(RIGHT)
-//BORRAR LAS 3 DE LA IZQUIERDA Y AGREGAR 3 A LA DERECHA
-//VERIFICAR QUE SEA MAYOR A 32 (De esa línea en específico) PARA QUE SE QUEDE EN LA MISMA LÍNEA
-@LOOP
-0;JMP
+(BARERASE) //105
+    @4 //Dirección guía barra
+    A=M
+    M=0
+    @4 //Dirección guía barra
+    M=M+1
+    @6 //Dato por columna a borrar
+    M=M-1
+    D=M
+    @NEXTERASE
+    D;JEQ
+    @BARERASE
+    0;JMP
+
+(NEXTERASE) //117
+    @29 // 32-3
+    D=A
+    @4 //Dirección guía barra
+    M=M+D
+    @3
+    D=A
+    @6 //Dato por columna a dibujar
+    M=D
+    @5 //Dato por línea a dibujar
+    M=M-1
+    D=M
+    @BARERASE
+    D;JGT // Finalización del loop
+    // Restablecimiento de valores para borrar la barra
+    @3
+    D=A
+    @5 //Dato por línea a borrar
+    M=D
+    @6 //Dato por columna a borrar
+    M=D
+    @BAR
+    0;JMP
